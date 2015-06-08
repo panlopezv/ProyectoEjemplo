1c45ea7e1fbdcf6587ed2fb3b4a475b7f44c19d8
est1629311 <est1629311@192.168.56.1>
2015-06-08 10:49:26 -0600

    Se agrego saludo

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -17,6 +17,7 @@ public class Prueba {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo!");
+        System.out.println("Hello World!");
     }
     
 }
